95a7966c683ffede523aa3361d17e537ac036b89
Daniel <daniel.hakimi@ibood.com>
2022-06-21 00:47:58 +0200

    added details page provider

---
+++ lib/providers/details_page_provider.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import '../enums/notifier_state.dart';
+
+import '../models/art_objects.dart';
+import '../models/response_failure.dart';
+import '../services/museum_data_service.dart';
+import 'home_page_provider.dart';
+
+class DetailsPageProvider extends ChangeNotifier {
+  MuseumDataService dataService;
+
+  Either<ResponseFailure, ArtObjects> _artItem = Right(ArtObjects());
+  Either<ResponseFailure, ArtObjects> get artItem => _artItem;
+
+  DetailsPageProvider(this.dataService);
+  NotifierState _state = NotifierState.inital;
+  NotifierState get state => _state;
+  set state(NotifierState state) {
+    _state = state;
+    notifyListeners();
+  }
+
+  Future<void> getArtItemBy({String? objectNumber}) async {
+    if (state == NotifierState.loading) {
+      return;
+    }
+    state = NotifierState.loading;
+
+    await Task(() => dataService.getCollectionDetails(
+              objectNumber: objectNumber ?? '',
+            ))
+        .unwrapResponse()
+        .then(
+          (value) => value.fold(
+            (l) => _artItem = Left(l),
+            (r) => _artItem = Right(r.artObject),
+          ),
+        )
+        .whenComplete(() => state = NotifierState.loaded);
+  }
+}
